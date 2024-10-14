5603e4c8272650356cd844078f28bf99f937e4ed
aliarmaganuygun <aau@eskisehir.edu.tr>
2024-10-14 10:58:57 +0300

    Home Page and Product Details Page UI has been done.

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Diagnostics;
+using BookBazaar.DataAccess.Repository.IRepository;
 using BookBazaar.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +10,23 @@ namespace BookBazaar.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(p=> p.Id == productId,includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
