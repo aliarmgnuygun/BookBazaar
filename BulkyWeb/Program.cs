4956ea9d6c04bd1d209fb315eca0dddcdc22cb90
aliarmaganuygun <aau@eskisehir.edu.tr>
2024-10-14 14:41:00 +0300

    The roles required for the application were defined and created in the database.

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,8 +1,10 @@
 using BookBazaar.DataAccess.Data;
 using BookBazaar.DataAccess.Repository;
 using BookBazaar.DataAccess.Repository.IRepository;
-using Microsoft.EntityFrameworkCore;
+using BookBazaar.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
