59bdb91d5e84bd142965f566d0fec1408b74796c
aliarmaganuygun <aau@eskisehir.edu.tr>
2024-10-28 18:35:29 +0300

    Lock/Unlock button added for users

--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -21,6 +21,29 @@ namespace BookBazaar.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                // User is currently locked, we will unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
